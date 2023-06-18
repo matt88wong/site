f888b879e5d276e2d49da4f3e35116934a37346c
Matt <mwong021400@gmail.com>
2023-06-18 17:16:37 +1200

    Adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { Home } from './Pages/Home';
+import { Home, Music } from './Pages';
+import { Navbar } from './Components/Navbar';
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<div>create about page!</div>} />
+          <Route path="portfolio" element={<div>create portfolio page!</div>} />
+          <Route path="professional" element={<div>create professional page!</div>} />
+          <Route path="music" element={<Music/>} />
+          <Route path="food" element={<div>create food page!</div>} />
+          <Route path="football" element={<div>create football page!</div>} />
+      </Routes>
+      <Outlet />    
+    </BrowserRouter>
   );
 }
 
