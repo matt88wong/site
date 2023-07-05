54359c9a7c615f60f6bed1b22d642bd1b110f817
Matt <mwong021400@gmail.com>
2023-07-05 21:20:26 +1200

    Moving body styling to app.scss file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import { Home, Music } from './Pages';
-import { Navbar } from './Features';
+//Dependencies
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+
+//Pages
+import { Home, Music } from './Pages';
+
+//Features
+import { Navbar } from './Features';
+
+//Style
+import './css/app.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<div>create about page!</div>} />
-          <Route path="portfolio" element={<div>create portfolio page!</div>} />
-          <Route path="professional" element={<div>create professional page!</div>} />
-          <Route path="music" element={<Music/>} />
-          <Route path="food" element={<div>create food page!</div>} />
-          <Route path="football" element={<div>create football page!</div>} />
-      </Routes>
-      <Outlet />    
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<div>create about page!</div>} />
+            <Route path="portfolio" element={<div>create portfolio page!</div>} />
+            <Route path="professional" element={<div>create professional page!</div>} />
+            <Route path="music" element={<Music/>} />
+            <Route path="food" element={<div>create food page!</div>} />
+            <Route path="football" element={<div>create football page!</div>} />
+        </Routes>
+        <Outlet />    
+      </BrowserRouter>
+    </div>
   );
 }
 
