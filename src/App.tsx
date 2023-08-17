5bc1581772cd21bacc1b5d1c090eae5a7100fed2
Matt <mwong021400@gmail.com>
2023-08-17 22:16:12 +1200

    Update ambigious route go to home page

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
           <Route path={TextResources.pageLinks.portfolio.pageLink} element={<Portfolio/>} />
           <Route path={TextResources.pageLinks.blog.pageLink} element={<Blog />} />
         </Routes>
